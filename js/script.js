bb50bdc60cd3659ed8b5ad2b1097cf87039760ae
WistRu <wild1990@mail.ru>
2020-07-20 21:38:55 +0300

    20

--- js/script.js
+++ js/script.js
@@ -1,5 +1,63 @@
 "use strict";
 
+const obj = new Object();
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    }
+};
+
+// console.log(options.name);
+
+// delete options.name;
+
+// console.log(options);
+
+let counter = 0;
+for (let key in options) {
+    if (typeof(options[key]) === 'object') {
+        for (let i in options[key]) {
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+            counter++;
+        }
+    } else {
+        console.log(`Свойство ${key} имеет значение ${options[key]}`);
+        counter++;
+    }
+}
+console.log(counter);
+
+// console.log(options["colors"]["border"]);
+
+
+
+// function first(){
+//     // do something
+//     setTimeout(function() {console.log(1);}, 500);
+// }
+
+// function second() {
+//     console.log(2);
+// }
+
+// first();
+// second();
+
+// function learnJS(lang, callback) {
+//     console.log(`Я учу: ${lang}`);
+//     callback();
+// }
+
+// function done() {
+//     console.log('Я прошел этот урок')
+// }
+
+// learnJS('JavaScript', done);
+
 // const str = "teSt";
 // const arr = [1, 2, 3];
 
