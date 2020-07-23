00bb2cb1eddf018eb2ecc6a2369466c294574d93
WistRu <wild1990@mail.ru>
2020-07-23 21:24:13 +0300

    26

--- js/exercises.js
+++ js/exercises.js
@@ -1,3 +1,49 @@
+// to String
+//1
+console.log(String(null));
+console.log(String(4));
+//2
+console.log(typeof(5+""));
+const num = 5;
+console.log("http://vk.com/catalog/" + num);
+
+const fontSize = 26 + 'px';
+
+//to Number
+//1
+console.log(typeof(Number('4')));
+
+//2
+console.log(typeof(+'4'));
+
+//3
+console.log(typeof(parseInt('15px', 10)));
+
+let answ = +prompt('hi', "");
+
+//to boolean
+
+// 0, '', null, undefined, NaN - false;
+
+//1
+let switcher = null;
+if (switcher) {
+    console.log('Working...');
+}
+
+switcher = 1;
+
+if (switcher) {
+    console.log('Working...');
+}
+
+//2
+console.log(typeof(Boolean('4')));
+
+//3
+console.log(typeof(!!"444"));
+
+
 // let str = 'some';
 // let strObj = new String(str);
 
